e5fc6cde486e3f17ed7bcf8d34802f04d9652f38
Qing Tang <zucchini.tang@gmail.com>
2019-04-26 13:42:34 +0900

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,19 @@
+from .vgg import *
+# from .dpn import *
+# from .lenet import *
+# from .senet import *
+# from .pnasnet import *
+# from .densenet import *
+# from .googlenet import *
+# from .shufflenet import *
+# from .shufflenetv2 import *
+# from .resnet import *
+# from .resnext import *
+# from .preact_resnet import *
+# from .mobilenet import *
+# from .mobilenetv2 import *
+from .vgg_occ_v1 import *
+from .vgg_occ_v2 import *
+from .vgg_occ_v3 import *
+from .vgg_occ_v4_1 import *
+from .vgg_occ_v4_0_4 import *
---
+++ models/vgg_occ_v3.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+cfg = {
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG_OCC_v4(nn.Module):
+    def __init__(self, vgg_name):
+        super(VGG_OCC_v4, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        self.classifier = nn.Linear(512, 10)   # fully-connect nn.Linear(in_features, out_features)
+        self.classifier_occluded = nn.Linear(512,2)
+        #self.activate = nn.ReLU(inplace=True)
+    def forward(self, x):
+        out = self.features(x)     # out:[1, 512, 1, 1]   [number of image, image_size, _, _]
+        #print('1',out.shape)
+        out = out.view(out.size(0), -1)  # out:[1, 512]
+        #print('2',out.shape)
+        out_confidence = self.classifier(out)    # out:[1, 10]
+
+        out_occluded = self.classifier_occluded(out)     # out:[1, 2]
+        #out_occluded = self.activate(out_occluded)
+        out = torch.cat((out_occluded,out_confidence),1) 
+        #print('3',out.shape)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+#                 np_M_shape = np.shape(layers)
+#                 print('np_M_shape:',np_M_shape)
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+#                 np_shape = np.shape(layers)
+#                 np_size = np.size(layers)
+#                 print('np_shape:',np_shape,'np_size',np_size)
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+        
+# def test():
+#     net = VGG_OCC('VGG11')
+#     x = torch.randn(1,3,32,32)
+#     y = net(x)
+#     print('node_12:  ',y.size())
+    
+# #test()
